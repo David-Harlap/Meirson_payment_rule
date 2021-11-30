6b6d2ecf04f94fb0c7fc0e69c6ee5bbc0d308436
DavidHarlap <adavid12@gmail.com>
2021-11-30 09:50:42 +0200

    delete and change args

--- meirson.py
+++ meirson.py
@@ -2,10 +2,6 @@ import unittest
 from typing import List
 
 
-def choice_rule1(values: list[float]) -> list[bool]:
-    return None
-
-
 def payments(values: List[float], func) -> List[float]:
     pay = []
     c = func(values)
@@ -42,9 +38,9 @@ def greedy_a(values: List[float]) -> List[bool]:
 
 class MyTestCase(unittest.TestCase):
     def test_more_then_ten(self):
-        l = [12, 5, 7, 10, 9, 13, 9.95, 10.1]
-        print(l)
-        answer = payments(l, more_then_ten)
+        l1 = [12, 5, 7, 10, 9, 13, 9.95, 10.1]
+        print(l1)
+        answer = payments(l1, more_then_ten)
         print(answer)
         self.assertAlmostEqual(float(answer[0]), 10, delta=0.0001)
         self.assertAlmostEqual(float(answer[3]), 10, delta=0.0001)
@@ -52,14 +48,14 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(float(answer[7]), 10, delta=0.0001)
 
     def test_greedy_a(self):
-        l = [12, 5, 7, 10, 9, 13, 9.95, 10.1]
-        print(l)
-        answer = payments(l, greedy_a)
+        l1 = [12, 5, 7, 10, 9, 13, 9.95, 10.1]
+        print(l1)
+        answer = payments(l1, greedy_a)
         print(answer)
         self.assertAlmostEqual(float(answer[5]), 12, delta=0.0001)
-        l1 = [45, 45.2, 44.8, 52, 51.85, 51.87, 51.8]
-        print(l1)
-        answer1 = payments(l1, greedy_a)
+        l2 = [45, 45.2, 44.8, 52, 51.85, 51.87, 51.8]
+        print(l2)
+        answer1 = payments(l2, greedy_a)
         print(answer1)
 
 
